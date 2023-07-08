4d85c08da94096a6efdf73d351c5637250f0e201
Melvin-Jovano <melvinjovano2@gmail.com>
2023-07-08 22:51:06 +0700

    feat: home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
+import 'package:mobile_uas/config/days.dart';
 import 'package:mobile_uas/config/pallete.dart';
 import 'package:mobile_uas/utils/hour_rotation.dart';
 import 'package:mobile_uas/utils/rand_int.dart';
@@ -19,6 +20,13 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+
+          },
+          backgroundColor: const Color(0xFF2D2D2D),
+          child: Image.asset('assets/img/news.png'),
+        ),
         backgroundColor: Pallete.primary,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
@@ -38,37 +46,39 @@ class _HomePageState extends State<HomePage> {
           ),
           child: ListView(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      const Text('27°', style: TextStyle(
-                        fontSize: 44
-                      ),),
-                      const SizedBox(width: 10,),
-                      Column(
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 10,),
-                          const Text('33° / 24°', style: TextStyle(
-                            fontSize: 12
-                          ),),
-                          Text(DateFormat('EEEE, HH:mm').format(DateTime.now()), style: const TextStyle(
-                            fontSize: 12
-                          ),),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Image.asset('assets/img/full_moon.png')
-                ],
+              Container(
+                margin: const EdgeInsets.only(bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Text('27°', style: TextStyle(
+                          fontSize: 44
+                        ),),
+                        const SizedBox(width: 10,),
+                        Column(
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 10,),
+                            const Text('33° / 24°', style: TextStyle(
+                              fontSize: 12
+                            ),),
+                            Text(DateFormat('EEEE, HH:mm').format(DateTime.now()), style: const TextStyle(
+                              fontSize: 12
+                            ),),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Image.asset('assets/img/full_moon.png')
+                  ],
+                ),
               ),
 
-              const SizedBox(height: 10,),
-
               Container(
+                margin: const EdgeInsets.only(bottom: 20),
                 decoration: const BoxDecoration(
                   color: Colors.white10,
                   borderRadius: BorderRadius.all(Radius.circular(20))
@@ -136,9 +146,167 @@ class _HomePageState extends State<HomePage> {
                             ],
                           )
                         ],
-                      )
+                      ),
                     ],
                   ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(bottom: 20),
+                decoration: const BoxDecoration(
+                  color: Colors.white10,
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                ),
+                child: const Center(
+                  child: Column(
+                    children: [
+                      Text('Six Hour Storm Outlook', style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16
+                      ),),
+                      SizedBox(height: 10,),
+                      Text('Storms ending around 09:00 pm', style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFB1B1B1)
+                      ),),
+                      SizedBox(height: 30,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.circle, color: Colors.white, size: 10,),
+                          SizedBox(width: 10,),
+                          Icon(Icons.circle, color: Color(0xFF6B6B6B), size: 10,),
+                          SizedBox(width: 10,),
+                          Icon(Icons.circle, color: Color(0xFF6B6B6B), size: 10,),
+                          SizedBox(width: 10,),
+                          Icon(Icons.circle, color: Color(0xFF6B6B6B), size: 10,),
+                        ],
+                      )
+                    ],
+                  )
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(bottom: 20),
+                decoration: const BoxDecoration(
+                  color: Colors.white10,
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                  horizontal: 20
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Yesterday', style: TextStyle(
+                          color: Color(0xFF6B6B6B),
+                          fontWeight: FontWeight.bold
+                        ),),
+                        Text('${randInt(30, 35)}°${randInt(20, 25)}°', style: const TextStyle(
+                          color: Color(0xFF6B6B6B),
+                          fontWeight: FontWeight.bold
+                        ),),
+                      ],
+                    ),
+                    const SizedBox(height: 20,),
+                    for(int i = 0; i < 7; i++)
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: Text(i == DateTime.now().weekday-1 ? 'Today' : days[i], style: const TextStyle(
+                            fontWeight: FontWeight.bold
+                          ),),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Row(
+                            children: [
+                              const Icon(Icons.water_drop, size: 9, color: Colors.white,),
+                              const SizedBox(width: 2,),
+                              Text('${randInt(30, 80)}%', style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 11
+                              ),),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Image.asset('assets/img/thunder.png')
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Image.asset('assets/img/thunder.png')
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text('${randInt(30, 35)}°${randInt(20, 25)}°', style: const TextStyle(
+                            fontWeight: FontWeight.bold
+                            ),),
+                          )
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(bottom: 20),
+                decoration: const BoxDecoration(
+                  color: Colors.white10,
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 20
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          const Text('Sunrise', style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),),
+                          const SizedBox(height: 10,),
+                          const Text('06:14', style: TextStyle(
+                            fontWeight: FontWeight.bold
+                          ),),
+                          const SizedBox(height: 20,),
+                          Image.asset('assets/img/sunrise.png')
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          const Text('Sunset', style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),),
+                          const SizedBox(height: 10,),
+                          const Text('18:30', style: TextStyle(
+                            fontWeight: FontWeight.bold
+                          ),),
+                          const SizedBox(height: 20,),
+                          Image.asset('assets/img/sunset.png')
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               )
             ],
